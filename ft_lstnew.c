b971fe0a4cedb0a970b5ae1b5fa46fe59f3bec46
mpaju <mikkpaju@gmail.com>
2017-01-09 23:06:36 +0200

    a

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -2,5 +2,11 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	
+	t_list	*link;
+	if (!(link = (t_list *)malloc(sizeof(*link))))
+		return (NULL);
+	link->content = content;
+	link->content_size = content_size;
+	link->next = NULL;
+	return (&link);
 }
